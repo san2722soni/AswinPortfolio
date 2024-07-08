9360af0f4bba889db4822525d0b68f66a3e72f6a
Aswin <invictusasw7@gmail.com>
2024-07-08 11:27:51 +0530

    fixed services card link

--- app/components/services.tsx
+++ app/components/services.tsx
@@ -39,37 +39,37 @@ export const projects = [
     title: <SkillCard desc="C++" limit={80} duration="2s" fill={120} icon={'CPP'}/>,
     description:
     "Skilled in C++, with proficiency in data structures and algorithms, I've developed robust applications and optimized performance, demonstrating expertise in efficient algorithms, object-oriented programming, and memory management.",
-    link: "https://netflix.com",
+    link: "",
   },
   {
     title: <SkillCard desc="React" limit={70} duration="2s" fill={130} icon={'REACT'}/>,
     description:
     "Skilled in Next.js and React, I've developed dynamic web applications and created interactive user interfaces, showcasing expertise in server-side rendering, seamless client-side navigation, component-based architecture, and state management.",
-    link: "https://google.com",
+    link: "",
   },
   {
     title: <SkillCard desc="REDIS"  limit={65} duration="2s" fill={140} icon={'REDIS'}/>,
     description:
     "Experienced with Prisma and Redis, I've built scalable database solutions and implemented efficient caching strategies, showcasing expertise in ORM integration, data modeling, key-value store optimization, and data persistence.",
-    link: "https://meta.com",
+    link: "",
   },
   {
     title: <SkillCard desc="TS" limit={75} duration="2s" fill={120} icon={'TS'}/>,
     description:
     "Proficient in JavaScript and TypeScript, I've developed dynamic web applications and scalable solutions, showcasing expertise in frontend and backend development, type safety, asynchronous programming, and modern JavaScript development.",
-    link: "https://amazon.com",
+    link: "",
   },
   {
     title: <SkillCard desc="NODE" limit={70} duration="2s" fill={145} icon={'NODE'}/>,
     description:
     "Skilled in Node.js and cloud technologies, I've built server-side applications and deployed scalable solutions, showcasing expertise in asynchronous programming, RESTful APIs, cloud architecture, and serverless computing.",
-    link: "https://microsoft.com",
+    link: "",
   },
     {
     title: <SkillCard desc="HTML" limit={90} duration="2s" fill={80} icon={'HTML'}/>,
     description:
     "Experienced in HTML and CSS, I've crafted responsive and visually appealing web interfaces, showcasing expertise in semantic markup, styling techniques, layout design, and cross-browser compatibility.",
-    link: "https://stripe.com",
+    link: "",
   },
   
 ];
